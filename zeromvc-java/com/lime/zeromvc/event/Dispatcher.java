9c1a8c3340a6748f351e0735f0d6defe541dabfe
linming <wc24@qq.com>
2014-10-21 19:05:49 +0800

    ff

---
+++ zeromvc-java/com/lime/zeromvc/event/Dispatcher.java
@@ -0,0 +1,11 @@
+package com.lime.zeromvc.event;
+
+/**
+ * Created by linming on 14-10-21.
+ */
+public class Dispatcher {
+    public Dispatcher(IDispatcher target) {
+    }
+    public Dispatcher() {
+    }
+}
---
+++ zeromvc-java/com/lime/zeromvc/event/Report.java
@@ -0,0 +1,7 @@
+package com.lime.zeromvc.event;
+
+/**
+ * Created by linming on 14-10-21.
+ */
+public class Report {
+}
